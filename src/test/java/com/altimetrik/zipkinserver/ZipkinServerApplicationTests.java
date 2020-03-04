28f8db3b24927dc08b771d6c48b7187fd2e8a4b3
madhankumar <madhan2007.psgtech@gmail.com>
2020-03-04 17:38:24 +0530

    first commit

---
+++ src/test/java/com/altimetrik/zipkinserver/ZipkinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.zipkinserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
